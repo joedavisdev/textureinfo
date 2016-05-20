394d174f450d2792d9dc4d1fb53fa2576a112681
Joe Davis <joedavisdev@gmail.com>
2016-05-21 00:05:28 +0100

    TravisCI: Hmm. This one works locally o_O

--- .travis_build.sh
+++ .travis_build.sh
@@ -1,7 +1,8 @@
 set -e
 # Do some setup
 CMAKE_BIN=
-if [[ "$1" == "linux" ]]; then
+echo "DEBUG: TravisCI host OS = $1"
+if [ "$1" == "linux" ]; then
 bash -c "cd ~/
 wget  --no-check-certificate https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz 
 tar -xf cmake-3.5.2-Linux-x86_64.tar.gz"
