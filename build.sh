ed1a257ca3a07e2d22f190702230c2bf2e0aa0f3
Joe Davis <joedavisdev@gmail.com>
2016-05-21 00:12:47 +0100

    TravisCI: Use the specified make binary instead of the system's make binary TravisCI: Print the name of the cmake binary the build is using

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ CMAKE_BIN=cmake
 if [ -z "$1" ]; then
     CMAKE_BIN=$1
 fi
+echo "DEBUG: CMake binary = $CMAKE_BIN"
 mkdir -p build
 cd build
-cmake ..
-cmake --build .
+$CMAKE_BIN ..
+$CMAKE_BIN --build .
 cd ..
