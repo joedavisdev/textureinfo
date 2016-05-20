ce40822f617d5e79a68e6a9f746939e89c189629
Joe Davis <joedavisdev@gmail.com>
2016-05-20 23:26:36 +0100

    TravisCI: For Linux builds, pull & use a newer Cmake binary package from cmake.org (ideas stolen from Paul Sobek: https://github.com/PaulSobekIMG/PVRApiIntro/commit/00e1b9a29e191a61b71f00a1cc1ecafc2b4e5402)

---
+++ .travis_build.sh
@@ -0,0 +1,10 @@
+# Do some setup
+CMAKE_BIN=
+if [ "$1" == "linux" ]; then
+bash -c "cd ~/
+wget  --no-check-certificate https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz 
+tar -xf cmake-3.5.2-Linux-x86_64.tar.gz"
+CMAKE_BIN=~/cmake-3.5.2-Linux-x86_64/bin/./cmake
+fi
+# Run the CMake build commands
+echo ./build.sh $CMAKE_BIN
--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+CMAKE_BIN=cmake
+if [-z $1]
+    CMAKE_BIN=$1
+fi
 set -e
 mkdir -p build
 cd build
