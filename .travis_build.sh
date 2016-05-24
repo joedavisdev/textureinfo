d9eda65789ce533b30e52817ea5bfd7c77c26cb5
Joe Davis <joedavisdev@gmail.com>
2016-05-24 20:19:46 +0100

    TravisCI: Update apt before calling other apt commands TravisCI: Only install and configure g++4.8 if the g++ compiler is active

--- .travis_build.sh
+++ .travis_build.sh
@@ -3,14 +3,17 @@ set -e
 CMAKE_BIN=
 echo "DEBUG: TravisCI host OS = $1"
 if [ "$1" == "linux" ]; then
+sudo apt-get update
 bash -c "cd ~/
 wget  --no-check-certificate https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz 
 tar -xf cmake-3.5.2-Linux-x86_64.tar.gz"
 CMAKE_BIN=~/cmake-3.5.2-Linux-x86_64/bin/./cmake
+if [ "$CXX" == "g++" ]; then
 sudo apt-get install g++-4.8
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
 sudo apt-get install libstdc++-4.8-dev
+fi
 fi
 # Run the CMake build commands
 bash build.sh $CMAKE_BIN
