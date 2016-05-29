7a3120c9277140e8f8f469fca9dda4c1c8273e54
Joe Davis <joedavisdev@gmail.com>
2016-05-29 18:56:31 +0100

    Feature: Default build is now in build/default/ so it can cleanly co-exist with other targets in the build/ dir

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 set -e
+ORIG_DIR=$PWD
 CMAKE_BIN=cmake
 if [ -n "$1" ]; then
     CMAKE_BIN=$1
 fi
 echo "DEBUG: CMake binary = $CMAKE_BIN"
-mkdir -p build
-cd build
-$CMAKE_BIN ..
+BUILD_DIR=build/default
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+$CMAKE_BIN $ORIG_DIR
 $CMAKE_BIN --build .
-cd ..
+cd $ORIG_DIR
+ 
